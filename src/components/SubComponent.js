d16ad1ddb721e7480b0a14c2161135aa66dabd9f
Samina-Haq <saminanaseem2019@gmail.com>
2022-08-02 19:31:35 -0700

    react

---
+++ src/components/SubComponent.js
@@ -0,0 +1,11 @@
+import './SubComponent.css'
+function  SubComponent(){
+    return (<div className='red'>
+
+        <p>This is a sub componenet</p>
+
+
+    </div>);
+}
+
+export default SubComponent;
